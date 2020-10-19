7eedd3b0dc81e6eb21e25216ed079353e4e743cf
Emil Oja <emil.oja@gmail.com>
2020-10-19 16:35:32 +0200

    chore: added find tracks by name, started implementing searchbar

--- src/main/java/se/experis/emiloj/itunesreplica/data_access/CustomerRepository.java
+++ src/main/java/se/experis/emiloj/itunesreplica/data_access/CustomerRepository.java
@@ -1,12 +1,10 @@
 package se.experis.emiloj.itunesreplica.data_access;
 
 import se.experis.emiloj.itunesreplica.models.Customer;
-import org.sqlite.jdbc4.JDBC4PreparedStatement;
 
 import java.math.BigDecimal;
 import java.sql.*;
 import java.util.*;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class CustomerRepository {
@@ -166,6 +164,52 @@ public class CustomerRepository {
         });
         return sortedMap;
     }
+
+    public ArrayList<String> getCustomersFavoriteGenre(int id) {
+
+        var tempMap = new HashMap<String, Double>();
+
+        try {
+            conn = DriverManager.getConnection(URL);
+            PreparedStatement prep =
+                    conn.prepareStatement("SELECT Genre.Name " +
+                            "FROM Genre INNER JOIN Track " +
+                            "ON Genre.GenreId=Track.GenreId " +
+                            "INNER JOIN InvoiceLine " +
+                            "ON Track.TrackId=InvoiceLine.TrackId " +
+                            "INNER JOIN Invoice " +
+                            "ON InvoiceLine.InvoiceId=Invoice.InvoiceId " +
+                            "INNER JOIN Customer " +
+                            "ON Invoice.CustomerId=?");
+            prep.setInt(1, id);
+            ResultSet set = prep.executeQuery();
+
+            while (set.next()) {
+                String name = set.getString("Name");
+                if (!tempMap.containsKey(name)) { // check if the country has no value in the map, if so set value to 1
+                    tempMap.put(name, 1.0);
+                } else { // else if key exists, add 1 to the linked value
+                    tempMap.put(name, tempMap.get(name) + 1.0);
+                }
+            }
+
+        } catch(Exception exception){
+            System.out.println(exception.toString());
+        }
+
+
+        var favorites = new ArrayList<String>();
+
+        favorites.add(sortMapByValue(tempMap).findFirst().get().getKey());
+        double temp = sortMapByValue(tempMap).findFirst().get().getValue();
+
+        if(temp == sortMapByValue(tempMap).skip(1).findFirst().get().getValue()){
+            favorites.add(sortMapByValue(tempMap).skip(1).findFirst().get().getKey());
+        }
+            return favorites;
+    }
+
+
 
     public Stream<Map.Entry<String, Double>> sortMapByValue(HashMap<String, Double> map) {
 
